593781eae1d9d73ac1910875fa17919d39cdeeaf
sumoncse2112 <107523112+sumoncse2112@users.noreply.github.com>
2024-02-07 23:37:49 +0600

    Update a_team.cpp

--- a_team.cpp
+++ a_team.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int p,v,t,n,total_count=0;
+    cin>>n;
+
+    for(int i=0; i<n;i++){
+        cin>>p>>v>>t;
+        int sum = p+v+t;
+        if(sum>=2){
+            total_count = total_count+1;
+        }
+    }
+    cout << total_count;
+    return 0;
+}
